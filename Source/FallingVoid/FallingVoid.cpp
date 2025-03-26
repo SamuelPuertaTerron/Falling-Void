f11cd0096d4ab1155c18103e983be4646eb4dcb3
Meathook563 <78488173+Meathook563@users.noreply.github.com>
2025-03-26 14:27:22 +0000

    Small code changes

--- Source/FallingVoid/FallingVoid.cpp
+++ Source/FallingVoid/FallingVoid.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright: Falling Void Studios
 
 #include "FallingVoid.h"
 #include "Modules/ModuleManager.h"
--- Source/FallingVoid/FallingVoid.h
+++ Source/FallingVoid/FallingVoid.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright: Falling Void Studios
 
 #pragma once
 
